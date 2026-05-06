c061e293e331d473fe4037225a4f5805dd774187
Abdallah Qusit <abdallah2512ahmed@gmail.com>
2026-05-06 23:20:59 +0300

    Main architecture of project

---
+++ Tyaran/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tyaran.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
